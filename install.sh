ce83ddc79cd36c0b37cdf82e9427577603e52c66
Chas Nelson <cvs@chasnelson.co.uk>
2020-04-12 16:47:05 +0100

    add systemd command for borg

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ ln -s ./config/kanshi/config ~/.config/kanshi/config
 ln -s ./config/redshift/redshift.conf ~/.config/redshift/redshift.conf
 ln -s ./config/sway/config.d/10-systemd ~/.config/sway/config.d/10-systemd
 ln -s ./config/sway/config ~/.config/sway/config
+ln -s ./config/systemd/user/borg_cjn-bak.service ~/.config/systemd/user/borg_cjn-bak.service
 ln -s ./config/waybar/config ~/.config/waybar/config
 ln -s ./config/waybar/style.css ~/.config/waybar/style.css
 ln -s ./config/artha.conf ~/.config/artha.conf
 ln -s ./local/bin/borg_cjn-bak.sh ~/.local/bin/borg_cjn-bak.sh
+systemctl enable --now --user borg_cjn-bak.service
