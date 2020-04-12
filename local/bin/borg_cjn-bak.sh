d5d64cccefc536cd77054673ddc7a79526d1d13d
Chas Nelson <cvs@chasnelson.co.uk>
2020-04-12 15:35:17 +0100

    make borg auto-update now works (no key yet)

--- local/bin/borg_cjn-bak.sh
+++ local/bin/borg_cjn-bak.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Setting this, so the repo does not need to be given on the commandline:
-export BORG_REPO=ssh://cn78d@beag-shuil.physics.gla.ac.uk/~/backup/main
+export BORG_REPO=/run/media/chas/CJN.bak/main
 
 # Setting this, so you won't be asked for your repository passphrase:
 export BORG_PASSPHRASE=''
