ad55a83274de2710979ba1a3a6859e59ad8786b2
Chas Nelson <cvs@chasnelson.co.uk>
2020-04-12 16:11:08 +0100

    add ability to create new borg repos

--- local/bin/borg_cjn-bak.sh
+++ local/bin/borg_cjn-bak.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
 # Setting this, so the repo does not need to be given on the commandline:
-export BORG_REPO=/run/media/chas/CJN.bak/main
+export BORG_REPO=/run/media/chas/CJN.bak/$HOSTNAME
 
 # Setting this, so you won't be asked for your repository passphrase:
-export BORG_PASSPHRASE=''
+# Note: we use a blank passphrase with a local keyfile that must be copied to another device manually if access is needed
+export BORG_PASSPHRASE=""
 
 # some helpers and error handling:
 info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
 trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
 
+if [ ! -d BORG_REPO ]; then
+    info "Backup repository doesn't exist! Creating new repository."
+    borg init                       \
+        --encryption keyfile        \
+        --verbose                   \
+        --show-rc                   \
+    
+    create_exit=$?
+    if [ ${create_exit} -eq 0 ]; then
+        info "Created repository successfully"
+    elif [ ${create_exit} -eq 1 ]; then
+        info "Creating repository finished with warnings"
+    else
+        info "Creating repository failed with errors"
+    fi
+fi
+
 info "Starting backup"
 
-# Backup the most important directories into an archive named after
-# the machine this script is currently running on:
+# Backup the most important directories into an archive named for now:
 
 borg create                         \
     --verbose                       \
@@ -27,7 +44,7 @@ borg create                         \
     --exclude '/var/cache/*'        \
     --exclude '/var/tmp/*'          \
                                     \
-    ::'{hostname}-{now}'            \
+    ::'{now}'                       \
     /etc                            \
     /home                           \
     /root                           \
@@ -37,18 +54,16 @@ backup_exit=$?
 
 info "Pruning repository"
 
-# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
-# archives of THIS machine. The '{hostname}-' prefix is very important to
-# limit prune's operation to this machine's archives and not apply to
-# other machines' archives also:
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly archives:
 
 borg prune                          \
     --list                          \
-    --prefix '{hostname}-'          \
     --show-rc                       \
     --keep-daily    7               \
     --keep-weekly   4               \
     --keep-monthly  6               \
+    --verbose                       \
+    --show-rc                       \
 
 prune_exit=$?
 
