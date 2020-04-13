50fe98124f3947cacf8d76497720782e78bcbe75
Chas Nelson <cvs@chasnelson.co.uk>
2020-04-13 12:53:15 +0100

    add default ranger config

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
 
+ROOT="$( dirname $( readlink -f $0 ) )"
+
 # create symlinks for files
 # note: assumes folders already exist
-ln -s ./.zshrc ~/.zshrc
-ln -s ./config/albert/albert.conf ~/.config/albert/albert.conf
-ln -s ./config/kanshi/config ~/.config/kanshi/config
-ln -s ./config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ./config/mako/config ~/.config/mako/config
-ln -s ./config/redshift/redshift.conf ~/.config/redshift/redshift.conf
-ln -s ./config/sway/config.d/10-systemd ~/.config/sway/config.d/10-systemd
-ln -s ./config/sway/config ~/.config/sway/config
-ln -s ./config/swaylock/config ~/.config/swaylock/config
-ln -s ./config/systemd/user/borg_cjn-bak.service ~/.config/systemd/user/borg_cjn-bak.service
-ln -s ./config/waybar/config ~/.config/waybar/config
-ln -s ./config/waybar/style.css ~/.config/waybar/style.css
-ln -s ./config/artha.conf ~/.config/artha.conf
-ln -s ./local/bin/borg_cjn-bak.sh ~/.local/bin/borg_cjn-bak.sh
+ln -sf $ROOT/.zshrc ~/.zshrc
+ln -sf $ROOT/config/albert/albert.conf ~/.config/albert/albert.conf
+ln -sf $ROOT/config/kanshi/config ~/.config/kanshi/config
+ln -sf $ROOT/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf $ROOT/config/mako/config ~/.config/mako/config
+ln -sf $ROOT/config/ranger/commands.py ~/.config/ranger/commands.py
+ln -sf $ROOT/config/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf $ROOT/config/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -sf $ROOT/config/ranger/scope.sh ~/.config/ranger/scope.sh
+ln -sf $ROOT/config/redshift/redshift.conf ~/.config/redshift/redshift.conf
+ln -sf $ROOT/config/sway/config.d/10-systemd ~/.config/sway/config.d/10-systemd
+ln -sf $ROOT/config/sway/config ~/.config/sway/config
+ln -sf $ROOT/config/swaylock/config ~/.config/swaylock/config
+ln -sf $ROOT/config/systemd/user/borg_cjn-bak.service ~/.config/systemd/user/borg_cjn-bak.service
+ln -sf $ROOT/config/waybar/config ~/.config/waybar/config
+ln -sf $ROOT/config/waybar/style.css ~/.config/waybar/style.css
+ln -sf $ROOT/config/artha.conf ~/.config/artha.conf
+ln -sf $ROOT/local/bin/borg_cjn-bak.sh ~/.local/bin/borg_cjn-bak.sh
+# note this will only work if the harddrive is plugged in
 systemctl enable --now --user borg_cjn-bak.service
