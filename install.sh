baf53c83629471e5ab25735334eae2ab465fea58
--get <github@chasnelson.co.uk>
2022-02-03 16:24:34 +0000

    feat: do massive update of install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-pikaur -Syu --needed - < pkglist.txt
+# pikaur -Syu --needed - < pkglist.txt
 
 ROOT="$( dirname $( readlink -f $0 ) )"
 
@@ -9,36 +9,42 @@ sudo localectl --no-convert set-x11-keymap gb numpad:microsoft
 
 # create symlinks for files
 # general
+ln -sf $ROOT/.xinitrc ~/.xinitrc
+ln -sf $ROOT/.Xresources ~/.Xresources
 ln -sf $ROOT/.zshrc ~/.zshrc
-ln -sf $ROOT/.zprofile ~/.zprofile
+
+ln -sf $ROOT/config/artha.conf ~/.config/artha.conf
+ln -sf $ROOT/config/betterlockscreenrc ~/.config/betterlockscreenrc
 ln -sf $ROOT/config/starship.toml ~/.config/starship.toml
+ln -sf $ROOT/config/zen.starship.toml ~/.config/zen.starship.toml
+
 mkdir -p ~/.config/alacritty
 ln -sf $ROOT/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 mkdir -p ~/.config/Code/User
 ln -sf $ROOT/config/Code/User/settings.json ~/.config/Code/User/settings.json
+
+mkdir -p ~/.config/dunst
+ln -sf $ROOT/config/dunst/dunstrc ~/.config/dunst/dunstrc
+
+mkdir -p ~/.config/i3
+ln -sf $ROOT/config/i3/config ~/.config/i3/config
+
+mkdir -p ~/.config/i3status
+ln -sf $ROOT/config/i3status/config ~/.config/i3status/config
+
 mkdir -p ~/.config/ranger
 ln -sf $ROOT/config/ranger/commands.py ~/.config/ranger/commands.py
 ln -sf $ROOT/config/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -sf $ROOT/config/ranger/rifle.conf ~/.config/ranger/rifle.conf
 ln -sf $ROOT/config/ranger/scope.sh ~/.config/ranger/scope.sh
+
 mkdir -p ~/.config/redshift
 ln -sf $ROOT/config/redshift/redshift.conf ~/.config/redshift/redshift.conf
-ln -sf $ROOT/config/artha.conf ~/.config/artha.conf
-sudo ln -sf $ROOT/config/clamav/clamd.conf /etc/clamav/clamd.conf
-sudo ln -sf $ROOT/config/clamav/freshclam.conf /etc/clamav/freshclam.conf
 
-# i3
-ln -sf $ROOT/.xinitrc ~/.xinitrc
-ln -sf $ROOT/.Xresources ~/.Xresources
-mkdir -p ~/.config/i3
-ln -sf $ROOT/config/i3/config ~/.config/i3/config
 mkdir -p ~/.config/rofi
 ln -sf $ROOT/config/rofi/config.rasi ~/.config/rofi/config.rasi
 ln -sf $ROOT/config/rofi/nord.rasi ~/.config/rofi/nord.rasi
-
-# notifications
-mkdir ~/.config/wired
-ln -sf $ROOT/config/wired/wired.ron ~/.config/wired/wired.ron
 
 # enable backup service
 # note this will only work if the harddrive is plugged in
@@ -57,8 +63,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # update AV and firewall
-freshclam
+sudo ln -sf $ROOT/config/clamav/clamd.conf /etc/clamav/clamd.conf
+sudo ln -sf $ROOT/config/clamav/freshclam.conf /etc/clamav/freshclam.conf
+sudo systemctl stop clamav-freshclam.service
+sudo freshclam
 sudo systemctl enable --now clamav-freshclam.service
+# if the following line fails, add --force
 sudo -u clamav /usr/bin/fangfrisch --conf /etc/fangfrisch/fangfrisch.conf initdb
 sudo systemctl enable --now fangfrisch.timer
 sudo systemctl enable --now clamav-daemon.service
