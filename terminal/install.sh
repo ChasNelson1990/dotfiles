4b5cb04d5cfef59341a5b193746eb1898335043f
Chas Nelson <github@chasnelson.co.uk>
2026-08-02 15:38:34 +0100

    feat: :sparkles: add dosfstools, gdu and nfs-utils to Storage
    All already explicitly installed -- dosfstools/nfs-utils are filesystem
    support tools matching the existing exfat-utils/ntfs-3g pattern; gdu is
    a disk usage analyzer alongside duf/dust.

--- terminal/install.sh
+++ terminal/install.sh
@@ -64,7 +64,7 @@ paru -S postgresql
 # install shell tools
 paru -S alacritty direnv mcfly oh-my-zsh-git starship tealdeer vim zsh
 # install file storage tools
-paru -S duf dust exfat-utils ntfs-3g plocate ranger rclone zip
+paru -S dosfstools duf dust exfat-utils gdu nfs-utils ntfs-3g plocate ranger rclone zip
 # ranger preview dependencies (images, video thumbnails, office documents)
 paru -S ueberzugpp ffmpegthumbnailer odt2txt
 # install system tools
